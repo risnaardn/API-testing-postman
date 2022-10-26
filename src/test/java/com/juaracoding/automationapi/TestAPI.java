6e296ac4df143a45668d9f8086b05ab283f9f0a7
Risna Putri Ramadhani <risnaardn@gmail.com>
2022-10-26 12:20:36 +0700

    add testing

---
+++ src/test/java/com/juaracoding/automationapi/TestAPI.java
@@ -0,0 +1,83 @@
+package com.juaracoding.automationapi;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class TestAPI {
+    String endpoint = "https://mern-backend-8881.herokuapp.com/products";
+    @Test
+    public void testGetList() {
+        Response response = RestAssured.get(endpoint);
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getStatusLine());
+        System.out.println(response.getHeader("content-type"));
+        System.out.println(response.getTime());
+
+        int actual = response.getStatusCode();
+        Assert.assertEquals(actual, 200);
+    }
+    @Test
+    public void testGetSingle() {
+        given()
+                .queryParam("api_key", "62e20e5bf0e24546b900e053")
+                .when()
+                .get("https://mern-backend-8881.herokuapp.com/products/62e20e5bf0e24546b900e053")
+                .then().statusCode(200);
+    }
+    @Test
+    public void testPut() {
+        JSONObject request = new JSONObject();
+        request.put("name", "Kulkas");
+        request.put("category", "Elektronik");
+        System.out.println(request.toJSONString());
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(request.toJSONString())
+                .when()
+                .put("https://mern-backend-8881.herokuapp.com/products/")
+                .then()
+                .statusCode(404)
+                .log().all();
+    }
+    @Test
+    public void testPatch() {
+        JSONObject request = new JSONObject();
+        request.put("name", "Kulkas");
+        request.put("category", "Elektronik");
+        System.out.println(request.toJSONString());
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(request.toJSONString())
+                .when()
+                .patch("https://mern-backend-8881.herokuapp.com/products/")
+                .then()
+                .statusCode(404)
+                .log().all();
+    }
+
+    @Test
+    public void testDelete() {
+
+        when()
+                .delete("https://mern-backend-8881.herokuapp.com/products/62e20a17f0e24546b900e037")
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
+}
